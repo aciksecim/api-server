ab717583e3eaabf45fd005fd1dad507ba0000298
ibrahimcagan <ibrahimcagan@gmail.com>
2019-06-06 00:14:18 +0300

    First Commit

---
+++ AcikSecim.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcikSecim.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcikSecim.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private WebApiDbContext _apiDbContext;
+        public ValuesController()
+        {
+            _apiDbContext = new WebApiDbContext();
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            List<User> users = _apiDbContext.Users.ToList();
+
+            return users;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AcikSecim.WebApi/Models/WebApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcikSecim.WebApi.Models
+{
+    public class WebApiDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "User ID=postgres; Password=1045246; Server=localhost; Port=5432; Database=AcikSecimDB; Integrated Security=true;");
+        }
+    }
+}
